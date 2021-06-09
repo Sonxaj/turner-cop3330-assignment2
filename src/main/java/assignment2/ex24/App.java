b0ea20f2b858557d8cb048991c454e6ac1bf0af9
Sonxaj <jonasturner12@gmail.com>
2021-06-08 21:17:05 -0400

    Initial commit

---
+++ src/main/java/assignment2/ex24/App.java
@@ -0,0 +1,64 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+/*
+Exercise 24 - Anagram Checker
+Using functions to abstract the logic away from the rest of your code makes it easier to read and easier to maintain.
+
+Create a program that compares two strings and determines if the two strings are anagrams.
+The program should prompt for both input strings and display the output as shown in the example that follows.
+
+Example Output
+
+Enter two strings and I'll tell you if they are anagrams:
+Enter the first string: note
+Enter the second string: tone
+"note" and "tone" are anagrams.
+
+Constraints
+
+Implement the program using a function called isAnagram, which takes in two words as its arguments and returns true
+or false. Invoke this function from your main program.
+Check that both words are the same length.
+
+Challenge
+
+Complete this program without using built-in language features. Use selection or repetition logic instead and
+develop your own algorithm.
+ */
+
+package assignment2.ex24;
+
+import java.util.Scanner;
+
+public class App {
+    Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App myApp = new App();
+        anagramChecker checkWords = new anagramChecker();
+
+        System.out.println("Enter two strings and I'll tell you if they are anagrams:\n");
+
+        String word1 = myApp.getInput(1);
+        String word2 = myApp.getInput(2);
+
+        boolean result = checkWords.isAnagram(word1, word2);
+
+        System.out.println(checkWords.displayResult(result, word1, word2));
+    }
+
+    private String getInput(int operation){
+        switch (operation){
+            case 1 -> System.out.println("Enter the first string:");
+            case 2 -> System.out.println("Enter the second string");
+        }
+        return  in.nextLine();
+    }
+}
+
+
+
+
---
+++ src/main/java/assignment2/ex24/anagramChecker.java
@@ -0,0 +1,32 @@
+package assignment2.ex24;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class anagramChecker {
+
+    public String displayResult(boolean isAnagramResult, String word1, String word2){
+        if(isAnagramResult){
+            return "\"" + word1 + "\"" + " and " + "\"" + word2 + "\" are anagrams.";
+        }else{
+            return "\"" + word1 + "\"" + " and " + "\"" + word2 + "\" are not anagrams.";
+        }
+    }
+
+    public boolean isAnagram(String word1, String word2){
+        boolean out = false;
+        word1 = sortedWord(word1);
+        word2 = sortedWord(word2);
+
+        if(word1.equalsIgnoreCase(word2))
+            out = true;
+
+        return out;
+    }
+
+    public String sortedWord(String word){
+        char[] wordToCheck = word.toLowerCase(Locale.ROOT).toCharArray(); //lowercase to simplify checks
+        Arrays.sort(wordToCheck); //sorted
+        return Arrays.toString(wordToCheck); //now a sorted string
+    }
+}
