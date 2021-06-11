81c35ba50eedcdcb01325e9d93079088ad456c2f
Sonxaj <jonasturner12@gmail.com>
2021-06-11 00:19:04 -0400

    completed ex25 (need to finish challenge though)

---
+++ src/main/java/assignment2/ex25/base/PasswordCheck.java
@@ -0,0 +1,50 @@
+package assignment2.ex25.base;
+
+public class PasswordCheck {
+
+    public int passwordValidator(String password){
+        char passArr[] = password.toCharArray();
+        char[] specialChars = new char[]{'!', '@', '#', '$', '%', '*', '?'}; // special characters i could think of atm
+
+        int result = 0;
+
+        for(int i=0; i<(passArr.length-1); i++){
+            // only numbers?
+            if(Character.isDigit(passArr[i]) && password.length() < 8){
+                result = 1;
+            }
+            // only letters?
+            if(Character.isLetter(passArr[i]) && password.length() < 8){
+                result = 2;
+            }
+            // both?
+            if((Character.isDigit(passArr[i]) || Character.isLetter(passArr[i])) && password.length() >= 8){
+                result = 3;
+            }
+        }
+
+        // special characters?
+        for(int i=0; i<(passArr.length-1); i++){
+            for(int j=0; j<(specialChars.length-1); j++){
+                if(Character.compare(passArr[i], specialChars[j]) == 0 && password.length() >= 8){
+                    result = 4;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public String validationResult(int strength, String password){
+        String out = "The password '" + password;
+
+        switch (strength){
+            case 1 -> out += "' is a very weak password.";
+            case 2 -> out += "' is a weak password.";
+            case 3 -> out += "' is a strong password.";
+            case 4 -> out += "' is a very strong password.";
+        }
+        return out;
+    }
+}
---
+++ src/test/java/assignment2/ex25/base/PasswordCheckTest.java
@@ -0,0 +1,56 @@
+package assignment2.ex25.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordCheckTest {
+    App testApp = new App();
+    PasswordCheck testPC = new PasswordCheck();
+
+    @Test
+    void passwordValidator_is_only_returning_a_1() {
+
+
+        int expected = 1;
+        int actual = testPC.passwordValidator("12345");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void passwordValidator_is_only_returning_a_2() {
+
+        int expected = 2;
+        int actual = testPC.passwordValidator("abcxyz");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void passwordValidator_is_only_returning_a_3() {
+
+        int expected = 3; // maximum expected
+        int actual = testPC.passwordValidator("abc123xyz");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void passwordValidator_is_only_returning_a_4() {
+
+        int expected = 4; // maximum expected
+        int actual = testPC.passwordValidator("1337h@xor");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void validationResult_prints_expected_text() {
+
+        String expected = "The password '1337h@xor!' is a very strong password.";
+        String actual = testPC.validationResult(testPC.passwordValidator("1337h@xor!"), "1337h@xor!");
+
+        assertEquals(expected, actual);
+    }
+}
