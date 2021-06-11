24bc5902bc71c81a1ae5881a800fa57545144c3c
Sonxaj <jonasturner12@gmail.com>
2021-06-11 00:20:14 -0400

    fixed header text

--- src/main/java/assignment2/ex24/base/App.java
+++ src/main/java/assignment2/ex24/base/App.java
@@ -1,6 +1,6 @@
 /*
- *  UCF COP3330 Summer 2021 Assignment 2 Solution
- *  Copyright 2021 first_name last_name
+ *  UCF COP3330 Summer 2021 Assignment 24 Solution
+ *  Copyright 2021 Jonas Turner
  */
 
 /*
