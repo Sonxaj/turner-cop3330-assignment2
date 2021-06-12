a7eb2f936d810bef489daf0deeef2a0149bc751d
Sonxaj <jonasturner12@gmail.com>
2021-06-12 00:13:05 -0400

    completed ex29 and ex30 (challenges unfinished)
    need to improve testing for 28 and 30

---
+++ src/test/java/assignment2/ex28/base/AppTest.java
@@ -0,0 +1,20 @@
+package assignment2.ex28.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void getInput() {
+    }
+
+    @Test
+    void genOut() {
+    }
+
+    @Test
+    void printOut() {
+    }
+}
